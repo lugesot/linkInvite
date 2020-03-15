49ba0ad33a9568f7258d12ea9c81c640d9fec221
田冬冬 <tdd@192.168.1.104>
2020-03-15 14:16:36 +0800

    update

--- runStep1.py
+++ runStep1.py
@@ -1,5 +1,7 @@
+import redis
 import time
 import os.path
+import logging,os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,30 +13,51 @@ def main():
     accountFile = "accounts.txt"
 
     ci = CollectInfo()
-    ci.readAccount(accountFile)
-    ci.login()
-    #等待页面过滤candidate操作结束
-    data = input("please enter any key to store candidate data info to the file: ")
-    #列表
-    #xpath https://www.cnblogs.com/liangblog/p/11943877.html
-    ci.searchPage(0)
-    #进入个人profile主页
-    #ci.profilePage()
-    #进入个人主页
-    #ci.mainPage()
-    #end
-    #ci.close()
-    ci.save()
+    if ci.testRedis() == True:
+        ci.readAccount(accountFile)
+        ci.login()
+        #等待页面过滤candidate操作结束
+        data = input("please enter any key to store candidate data info to the file: ")
+        #列表
+        #xpath https://www.cnblogs.com/liangblog/p/11943877.html
+        ci.logger.info("开始搜集：" + 0)   
+        try:
+            ci.searchPage(0)
+        except Exception as e:
+            print(e)
+            ci.logger.error(e)
+        else:
+            print("搜集完毕")
+        
+        ci.logger.info("本次共搜集个数：" + len(ci.data))   
+        ci.save()
 
 #element.close()
 class CollectInfo(object):
     def __init__(self):
+        self.logger = self.get_logger()
         options = webdriver.ChromeOptions()
         options.add_argument("--incognito") #隐身模式
         #driver_path = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"  # chromedriver.exe 的路径
         #driver = webdriver.Chrome(executable_path=driver_path, options=options)
         self.driver = webdriver.Chrome(options=options)
+        self.reidsClient = redis.Redis(host='127.0.0.1', port=6379)
+        #self.sharedConnectionsKey = "sharedConnections"
         self.data = []
+        
+
+    def testRedis(self):
+        try:
+            conn = redis.Redis(host='127.0.0.1', port=6379)
+            conn.set("test","test")
+        except Exception as e:
+            print("connecting to Redis -- 127.0.0.1:6379. Connection refused")
+            return False
+        else:
+            print("connecting to Redis -- 127.0.0.1:6379. Connection sucessfully!")
+            return True
+    pass
+
     #format username password\n
     def readAccount(self, fileName):
         self.accounts = []
@@ -53,7 +76,7 @@ class CollectInfo(object):
             if len(self.accounts)==0:
                 raise Exception("账户信息为空，请检查格式，每行: 用户名 空格 密码")
         except FileNotFoundError as fnf:
-            print("{0}文件不存在".format(fileName))
+            self.logger.debug("{0}文件不存在".format(fileName))
         pass
 
     def login(self):
@@ -61,7 +84,7 @@ class CollectInfo(object):
         password = self.accounts[0][1]
         time.sleep(0.5)
         self.driver.get("https://www.linkedin.com")
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(8)
         element = self.driver.find_element_by_name("session_key")
         element.send_keys(username)
         element = self.driver.find_element_by_name("session_password")
@@ -69,13 +92,20 @@ class CollectInfo(object):
         element.send_keys(Keys.RETURN)
 
     def searchPage(self,index):
+        self.logger.debug("搜索页面：" + index)
         self.driver.implicitly_wait(1)
         self.searchWindow = self.driver.window_handles[1] #.current_window_handle()
         self.driver.switch_to.window(self.searchWindow)
 
-        self.userList = self.driver.find_elements_by_xpath("//div[@id='search-results-region']//div[@class='top-card-info']//a")
-        for idx, j in enumerate(self.userList):
-            self.profilePage(idx)
+        # 当前页面target用户
+        #self.userList = self.driver.find_elements_by_xpath("//div[@id='search-results-region']//div[@class='top-card-info']//a")
+        userList = self.driver.find_elements_by_xpath("//div[@id='search-results-region']//li[@class='search-result']")
+        for idx, item in enumerate(userList):
+            viewsBtn = item.find_elements_by_xpath('.//button[contains(text(),"Views")]')
+            if len(viewsBtn)==0:
+                hrefItems = item.find_elements_by_xpath('.//div[@class="top-card-info"]//a')
+                self.profilePage(hrefItems[0])
+            # else: view的不添加
 
         # into next page
         nextPath = "//footer[@id='pagination-region']//div[@class='pagination-container']//li[@class='next']"
@@ -83,29 +113,34 @@ class CollectInfo(object):
         try:
             WebDriverWait(self.driver, 5, 0.5).until(EC.element_to_be_clickable(locator))
         except TimeoutException as te:
-            print("all pages end")
+            self.logger.debug("all pages end")
         else:
+            
             nextP = self.driver.find_elements_by_xpath(nextPath+"//a")
+            self.logger.debug("准备点击下一页")
             nextP[0].click()
+            self.logger.debug("点击了下一页")
             time.sleep(5)
             self.searchPage(index+1)
     
-    def profilePage(self,index):
-        urlEle = self.userList[index].get_attribute("href")
+    def profilePage(self,item):
+        urlEle = item.get_attribute("href")
         newWindow = "window.open('"+urlEle+"');"
         self.driver.execute_script(newWindow)
         toHandle = self.driver.window_handles[2]
         self.driver.switch_to.window(toHandle)
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(4)
         locator = (By.XPATH, '//div[@id="primary-content"]//div[@class="module-footer"]//a')
         try:
             WebDriverWait(self.driver, 5, 0.5).until(EC.element_to_be_clickable(locator))
         except TimeoutException as te:
-            print("skip who has no profile")
+            print("skip who has no profile："+urlEle)
+            self.logger.debug("skip who has no profile："+urlEle)
         else:
             profilePage = self.driver.find_element_by_xpath("//div[@id='primary-content']//div[@class='module-footer']//a")
             mainPageUrl = profilePage.get_attribute("href")
-            print(mainPageUrl)
+            print("添加用户:" + mainPageUrl)
+            self.logger.debug("添加用户:" + mainPageUrl)
             self.data.append({"mainPageUrl":mainPageUrl})
         #profilePage.click()
         self.driver.close()
@@ -114,23 +149,42 @@ class CollectInfo(object):
     def mainPage(self):
         toHandle = self.driver.window_handles[2]
         self.driver.switch_to.window(toHandle)
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(4)
         locator = (By.XPATH, '//main//section//button')
-        WebDriverWait(self.driver, 20, 0.5).until(EC.element_to_be_clickable(locator))
+        WebDriverWait(self.driver, 4, 0.5).until(EC.element_to_be_clickable(locator))
         try:
             self.driver.find_element_by_xpath('//main//section//button[contains(@class,"pv-s-profile-actions--connect")]')
         except Exception as e:
             print(e)
             print("connect btn not found")
+            self.logger.debug(e)
+            self.logger.debug("connect btn not found")
         else:
             print("connect btn found")
+            self.logger.debug("connect btn not found")
         self.driver.close()
     def close(self):
         data = input("input any key to end program ")
 
     def save(self):
+        self.logger.debug("保存文件开始:" + len(self.data))
         u = Util()
         u.createFile(self.data)
+        self.logger.debug("保存文件结束")
+
+    def get_logger(self):
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #获取当前目录的绝对路径
+        today = time.strftime("%Y%m%d", time.localtime())
+        filename = '/collect'+today +'.log'
+        log_dir = BASE_DIR + filename
+        fh = logging.FileHandler(log_dir,encoding='utf-8') #创建一个文件流并设置编码utf8
+        logger = logging.getLogger() #获得一个logger对象，默认是root
+        logger.setLevel(logging.DEBUG)  #设置最低等级debug
+        fm = logging.Formatter("%(asctime)s --- %(message)s")  #设置日志格式
+        logger.addHandler(fh) #把文件流添加进来，流向写入到文件
+        fh.setFormatter(fm) #把文件流添加写入格式
+        return logger
+    pass
     #########
 class Util(object):
     def createFile(self, dataList):
--- runStep2.py
+++ runStep2.py
@@ -1,25 +1,28 @@
 import time
 import os.path
+import urllib
+import logging,os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 import redis
 
 #搜索用户的账号
 def main():
 
     accounts = []
-    messageFile = "message.txt"
     accountFile = "accounts.txt"
+    
 
     si = SendInvitation(accounts)
     if si.testRedis() == True:
-        si.readMessage(messageFile)
         si.readAccount(accountFile)
-        si.inviteUserList()
+        si.swichNextUser()
+        si.iterateTarget()
         si.close()
 
 #element.close()
@@ -28,6 +31,8 @@ class SendInvitation(object):
         self.accountIndex = -1
         self.connectUserLimit = 200   #每个账户一天最多添加的用户数
         self.reidsClient = redis.Redis(host='127.0.0.1', port=6379)
+        self.sharedConnectionsKey = "sharedConnections"
+        self.logger = self.get_logger()
     
     #format username password\n
     def readAccount(self, fileName):
@@ -46,17 +51,23 @@ class SendInvitation(object):
             if len(self.accounts)==0:
                 raise Exception("账户信息为空，请检查格式，每行: 用户名 空格 密码")
         except FileNotFoundError as fnf:
-            print("linkin账号文件 {0} 不存在".format(fileName))
+            self.logger.debug("linkin账号文件 {0} 不存在".format(fileName))
             raise
         #print(self.accounts)
 
-    def readMessage(self, fileName):
-        d = os.getcwd()
-        fileName=d +'/'+fileName 
+    # def readMessage(self, fileName):
+    #     d = os.getcwd()
+    #     fileName=d +'/'+fileName 
+    #     self.message = ""
+    #     with open(fileName, 'r', encoding="utf-8") as file:
+    #         self.message = file.read()
+    #     print("邀请样式:" + self.message)
+    def readMessage(self,path,fileName):
+        fileName = path +'/'+fileName 
         self.message = ""
         with open(fileName, 'r', encoding="utf-8") as file:
             self.message = file.read()
-        print("邀请样式:" + self.message)
+        self.logger.debug("邀请样式:" + self.message)
 
     #领英限制：每个账户一天只能邀请100人
     def swichNextUser(self):
@@ -64,6 +75,7 @@ class SendInvitation(object):
         if self.accountIndex>=len(self.accounts):
             raise Exception("无account可用了，名额都已用尽")
         self.accountName = self.accounts[self.accountIndex][0]
+        self.logger.debug("已切换到用户：" + self.accountName)
 
         while self.checkLimitByRedis() == False:
             self.accountIndex = self.accountIndex +1
@@ -80,7 +92,7 @@ class SendInvitation(object):
         self.driver = webdriver.Chrome(options=options)
         time.sleep(0.5)
         self.driver.get("https://www.linkedin.com")
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(5)
         element = self.driver.find_element_by_name("session_key")
         element.send_keys(self.accountName)
         element = self.driver.find_element_by_name("session_password")
@@ -95,35 +107,38 @@ class SendInvitation(object):
         self.driver.execute_script(newWindow)
         toHandle = self.driver.window_handles[1]
         self.driver.switch_to.window(toHandle)
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(5)
 
         connectBtnXpath = '//main//section//button[contains(@class,"pv-s-profile-actions--connect")]'
         try:
             locator = (By.XPATH, connectBtnXpath)
             WebDriverWait(self.driver, 8, 0.5).until(EC.element_to_be_clickable(locator))
         except TimeoutException as te:
-            print("检查是否 是否通过more按钮 connect")
+            self.logger.debug("检查是否 是否通过more按钮 connect")
+            moreBtnXpath = '//main//section//span[@class="artdeco-button__text" and contains(text(),"More…") ]'
+            moreBtns = self.driver.find_elements_by_xpath('//span[@class="artdeco-button__text"]')
+            moreBtn = None
+            for btn in moreBtns:
+                if btn.text == "More…":
+                        moreBtn = btn
+                        break
+            if (moreBtn == None):
+                raise Exception("More button没找到: " + pageLink)
+            #moreBtn = moreBtn.parent
+            moreBtn.click()
+            time.sleep(0.5)
             try:
-                moreBtnXpath = '//button[@id="ember66"]'
-                locator = (By.XPATH, moreBtnXpath)
+                connPath = '//main//section//artdeco-dropdown-item[contains(@class,"pv-s-profile-actions--connect")]'
+                locator = (By.XPATH, connPath)
                 WebDriverWait(self.driver, 2, 0.5).until(EC.element_to_be_clickable(locator))
-            except TimeoutException as te2:
-                print("超时: " + pageLink)
+            except TimeoutException as te3:
+                self.logger.debug("已经是好友: " + pageLink)
             else:
-                moreBtn = self.driver.find_element_by_xpath(moreBtnXpath)
-                moreBtn.click()
-                try:
-                    connPath = '//main//section//artdeco-dropdown-item[contains(@class,"pv-s-profile-actions--connect")]'
-                    locator = (By.XPATH, connPath)
-                    WebDriverWait(self.driver, 2, 0.5).until(EC.element_to_be_clickable(locator))
-                except TimeoutException as te3:
-                    print("已经是好友: " + pageLink)
-                else:
-                    self.clickConnectBtn(connPath, pageLink)
+                self.clickConnectBtn(connPath, pageLink)
 
         except Exception as e:
-            print("异常发生："+e)
-            print("页面地址："+pageLink)
+            self.logger.debug("异常发生："+e)
+            self.logger.debug("页面地址："+pageLink)
         else:
             self.clickConnectBtn(connectBtnXpath, pageLink)
         finally:
@@ -131,7 +146,7 @@ class SendInvitation(object):
         pass
     
     def clickConnectBtn(self, btnXPah, pageLink):
-        #print("connect btn found")
+        #self.logger.debug("connect btn found")
         connectBtn = self.driver.find_element_by_xpath(btnXPah)
         connectBtn.click()
         time.sleep(2)
@@ -143,52 +158,82 @@ class SendInvitation(object):
         note.send_keys(self.message)
         time.sleep(1)
         sendInvitaionBtn = self.driver.find_element_by_xpath("//button[@aria-label='Send invitation']")
+        # 发出邀请
         sendInvitaionBtn.click()
         time.sleep(0.5)
         # 判断是否添加联系用户受限制
         try:
             limitBtn = self.driver.find_element_by_xpath("//button[@data-control-name='fuse_limit_got_it']")
         except NoSuchElementException:
+            # 成功发出邀请
             self.recordResult(self.accountName, pageLink)
             pass
-        # 如何limitBtn存在，则抛异常
-        raise InvitationLimitError(self.accountName, pageLink)
+        else :
+            self.logger.debug("!!!使用账号" + self.accountName + "添加受限，用户：" + pageLink)
+            # 如何limitBtn存在，则抛异常
+            raise InvitationLimitError(self.accountName, pageLink)
     
     def checkLimitByRedis(self):
         today = time.strftime("%Y%m%d", time.localtime())
         keyName = self.accountName + today
         if self.reidsClient.exists(keyName):
-            if ord(self.reidsClient.get("account20200223"))<=self.connectUserLimit:
+            if int(self.reidsClient.get(keyName))<=self.connectUserLimit:
                 return True
             else:
                 return False
+        else:
+            self.reidsClient.set(keyName, 0)
 
         return True
         pass
 
     def recordResult(self, userName, pageLink):
+        memberPublicId = self.getPublicIdFromLink(pageLink)
+        self.reidsClient.sadd(self.sharedConnectionsKey, memberPublicId)
+        self.logger.debug("发送邀请给用户:" + memberPublicId + " ~ " + pageLink)
+
         today = time.strftime("%Y%m%d", time.localtime())
         keyName = self.accountName + today
         if self.reidsClient.exists(keyName):
-            count = ord(self.reidsClient.incr(keyName))
+            count = int(self.reidsClient.incr(keyName))
             if count > self.connectUserLimit:
                 raise InvitationLimitError(userName, pageLink)
         else:
             self.reidsClient.set(keyName, 1)
         pass
     
-    def inviteUserList(self):
-        fileName = input("输入用户主页列表txt文件名（不需要后缀）:")
-        self.swichNextUser()
+    def iterateTarget(self):
         d = os.getcwd()
-        fileName=d +'/'+fileName + ".txt"
+        items = os.listdir(d)
+        for item in items:
+            path = os.path.join(d, item)
+            if os.path.isdir(path):
+                if item.startswith('target') == True:
+                    self.handleFolder(path)
+        pass
+
+    def handleFolder(self, path):
+        self.logger.debug("正在处理文件夹：" + path)
+        self.readMessage(path, "message.txt")
+        items = os.listdir(path)
+        for item in items:
+            fullPath = os.path.join(path, item)
+            if os.path.isdir(fullPath) == False:
+                if item.startswith('message') == False:
+                    self.inviteUserList(fullPath)
+        pass
+
+    def inviteUserList(self, fileName):
         try:
             with open(fileName, 'r', encoding="utf-8") as file:
+                self.logger.debug("正在处理文件：" + fileName)
                 while True:
                     userMainPageLink = file.readline()
                     if not userMainPageLink: 
                         break
                     userMainPageLink = userMainPageLink.strip()
+                    if self.isSharedMember(userMainPageLink) == True:
+                        continue
 
                     while True:
                         try:
@@ -200,30 +245,60 @@ class SendInvitation(object):
                         except Exception as e:
                             break
         except FileNotFoundError as fnf:
-            print("{0}文件不存在".format(fileName))
+            self.logger.debug("{0}文件不存在".format(fileName))
             raise 
         pass
+
+    def isSharedMember(self, userMainPageLink):
+        memberPublicId = self.getPublicIdFromLink(userMainPageLink)
+        isMember = self.reidsClient.sismember(self.sharedConnectionsKey, memberPublicId)
+        if isMember == True:
+            self.logger.debug("this is a shared member:" + memberPublicId)
+        return isMember
+    
+    def getPublicIdFromLink(self, userMainPageLink):
+        if userMainPageLink[-1] == '/':
+            userMainPageLink = userMainPageLink[0:-1]
+        arr = userMainPageLink.split("/")
+        memberPublicId = urllib.parse.unquote(arr[len(arr)-1])
+        return memberPublicId
 
     def testRedis(self):
         try:
             conn = redis.Redis(host='127.0.0.1', port=6379)
             conn.set("test","test")
         except Exception as e:
-            print("connecting to Redis -- 127.0.0.1:6379. Connection refused")
+            self.logger.debug("connecting to Redis -- 127.0.0.1:6379. Connection refused")
             return False
         else:
-            print("connecting to Redis -- 127.0.0.1:6379. Connection sucessfully!")
+            self.logger.debug("connecting to Redis -- 127.0.0.1:6379. Connection sucessfully!")
             return True
     pass
 
     def close(self):
         print("All end.")
+    
+    def get_logger(self):
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #获取当前目录的绝对路径
+        today = time.strftime("%Y%m%d", time.localtime())
+        filename = '/'+today +'.log'
+        log_dir = BASE_DIR + filename
+        fh = logging.FileHandler(log_dir,encoding='utf-8') #创建一个文件流并设置编码utf8
+        logger = logging.getLogger() #获得一个logger对象，默认是root
+        logger.setLevel(logging.DEBUG)  #设置最低等级debug
+        fm = logging.Formatter("%(asctime)s --- %(message)s")  #设置日志格式
+        logger.addHandler(fh) #把文件流添加进来，流向写入到文件
+        fh.setFormatter(fm) #把文件流添加写入格式
+        return logger
+
+pass
 
 class InvitationLimitError(Exception):
     def __init__(self,accountName, pageLink):  
         print("the account:{0} cannot invite anyone today. The user page now is {1}".format(accountName,pageLink))
         super().__init__()
     pass
+pass
 
 if __name__ == '__main__':
     main()
